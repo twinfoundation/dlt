ea15c275a715d91393cafd7f2b24ee23743c87eb
Martyn Janes <martyn@obany.com>
2025-01-16 09:45:58 +0200

    Minor fixes

--- packages/dlt-iota-rebased/tests/setupTestEnv.ts
+++ packages/dlt-iota-rebased/tests/setupTestEnv.ts
@@ -9,9 +9,11 @@ console.debug("Setting up test environment from .env and .env.dev files");
 dotenv.config({ path: [path.join(__dirname, ".env"), path.join(__dirname, ".env.dev")] });
 
 Guards.stringValue("TestEnv", "TEST_NODE_ENDPOINT", process.env.TEST_NODE_ENDPOINT);
+Guards.stringValue("TestEnv", "TEST_NETWORK", process.env.TEST_NETWORK);
 
 export const TEST_CLIENT_OPTIONS = {
 	url: process.env.TEST_NODE_ENDPOINT ?? ""
 };
 
 export const TEST_COIN_TYPE = 4218;
+export const TEST_NETWORK = process.env.TEST_NETWORK;
