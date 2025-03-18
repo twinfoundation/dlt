749284f094116ab22f85f18418d4a177b6fd53c0
Martyn Janes <martyn@obany.com>
2025-03-18 08:44:57 +0200

    Bump version

--- apps/move-to-json/src/cli.ts
+++ apps/move-to-json/src/cli.ts
@@ -29,7 +29,7 @@ export class CLI extends CLIBase {
 			{
 				title: "TWIN Move to JSON",
 				appName: "move-to-json",
-				version: "0.0.1-next.18",
+				version: "0.0.1-next.19",
 				icon: "⚙️ ",
 				supportsEnvFiles: false,
 				overrideOutputWidth: options?.overrideOutputWidth
