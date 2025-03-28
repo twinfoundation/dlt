eba6f490c7ccf43054a6611e9d0f46cedac4a9de
Martyn Janes <martyn@obany.com>
2025-03-28 16:25:57 +0200

    docs: add x-release-please-version in CLI

--- apps/move-to-json/src/cli.ts
+++ apps/move-to-json/src/cli.ts
@@ -29,7 +29,7 @@ export class CLI extends CLIBase {
 			{
 				title: "TWIN Move to JSON",
 				appName: "move-to-json",
-				version: "0.0.1-next.19",
+				version: "0.0.1-next.20", // x-release-please-version
 				icon: "⚙️ ",
 				supportsEnvFiles: false,
 				overrideOutputWidth: options?.overrideOutputWidth
